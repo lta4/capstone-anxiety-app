bd52d2ce3106d0702623e5cf2d08223ec95b1ff4
Andrew <andrew.mk4@yahoo.com>
2021-03-04 09:31:14 -0800

    added static pic

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,6 +9,9 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <StaticImage
+    src={`../images/lavender.jpg`} alt="Lavender"
+    />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
